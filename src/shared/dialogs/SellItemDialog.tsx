a6296062422bb7b7abca160e909dcc0d6a57f0b0
Kim Avillanosa <avillanosakzxc@gmail.com>
2023-10-07 19:08:09 +0800

    changed tz

--- src/shared/dialogs/SellItemDialog.tsx
+++ src/shared/dialogs/SellItemDialog.tsx
@@ -19,10 +19,10 @@ import { getEndDate, humanizeTimeLeftByDuration } from "../utils/humanizeDateTim
 const expirationOptions = [
     { label: "mock", value: 1 * 60 }, // 1 minute in milliseconds
     { label: "1 min", value: 1 * 60 }, // 1 minute in milliseconds
-    { label: "1 hour", value: 1 * 60 * 60 * 1000 }, // 1 hour in milliseconds
-    { label: "2 hours", value: 2 * 60 * 60 * 1000 }, // 2 hours in milliseconds
-    { label: "5 hours", value: 5 * 60 * 60 * 1000 }, // 5 hours in milliseconds
-    { label: "10 hours", value: 10 * 60 * 60 * 1000 }, // 10 hours in milliseconds
+    { label: "1 hour", value: 1 * 60 * 60 }, // 1 hour in milliseconds
+    { label: "2 hours", value: 2 * 60 * 60 }, // 2 hours in milliseconds
+    { label: "5 hours", value: 5 * 60 * 60 }, // 5 hours in milliseconds
+    { label: "10 hours", value: 10 * 60 * 60 }, // 10 hours in milliseconds
 ];
 
 const SellItemDialog: React.FC = () => {
